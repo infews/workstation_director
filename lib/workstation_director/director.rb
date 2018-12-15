c03785bf0dffa6272c7d471f2cfdc8161896f0bd
Davis W Frank <dwfrank@pivotal.io>
2018-12-15 12:57:27 -0800

    Adds reporting actors names
    Authored-by: Davis W Frank <dwfrank@pivotal.io>

--- lib/workstation_director/director.rb
+++ lib/workstation_director/director.rb
@@ -5,11 +5,18 @@ module WorkstationDirector
     end
 
     def action!
+      puts "Directing these actors: " + actor_names
       @actor_classes.each do |actor_class|
         actor = actor_class.new
         actor.install unless actor.present?
         actor.setup
       end
+    end
+
+    private
+
+    def actor_names
+      @actor_classes.collect{|klass| klass.to_s}.join(', ')
     end
   end
 end
--- spec/workstation_director/director_spec.rb
+++ spec/workstation_director/director_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe WorkstationDirector::Director do
   let(:actor_class) do
     actor_class = class_double('ActorClass')
     allow(actor_class).to receive(:new) {actor}
+    allow(actor_class).to receive(:to_s) {'ActorClass'}
     actor_class
   end
   let(:actor) do
@@ -12,8 +13,9 @@ RSpec.describe WorkstationDirector::Director do
     actor
   end
   let(:present_actor_class) do
-    actor_class = class_double('ActorClass')
+    actor_class = class_double('PresentActorClass')
     allow(actor_class).to receive(:new) {present_actor}
+    allow(actor_class).to receive(:to_s) {'PresentActorClass'}
     actor_class
   end
   let(:present_actor) do
@@ -25,42 +27,50 @@ RSpec.describe WorkstationDirector::Director do
   end
 
   context '#action!' do
+    before do
+      allow(STDOUT).to receive(:puts)
+      director.action!
+    end
+
     context 'with an actor' do
       let(:director) {WorkstationDirector::Director.new(actor_class)}
 
       context 'that returns false from #present?' do
 
-        it 'creates the actor' do
-          director.action!
+        it 'reports the actor(s) its running' do
+          expect(STDOUT).to have_received(:puts).with(/Directing these actor(s): ActorClass/)
+        end
 
+        it 'creates the actor' do
           expect(actor_class).to have_received(:new)
         end
 
         it 'tells the actor to install' do
-          director.action!
-
           expect(actor).to have_received(:install)
         end
 
         it 'tells the actor to setup' do
-          director.action!
-
           expect(actor).to have_received(:setup)
         end
       end
 
       context 'that returns true from #present?' do
         let(:director) {WorkstationDirector::Director.new(present_actor_class)}
-        it 'does not tell an actor to install' do
-          director.action!
 
+        it 'reports the actor(s) its running' do
+          expect(STDOUT).to have_received(:puts).with(/Directing these actor(s): PresentActorClass/)
+        end
+
+        it 'creates the actor' do
+          expect(present_actor_class).to have_received(:new)
+        end
+
+        it 'does not tell an actor to install' do
           expect(present_actor).to have_received(:present?)
           expect(present_actor).to_not have_received(:install)
         end
 
         it 'tells the actor to setup' do
-          director.action!
-
           expect(present_actor).to have_received(:setup)
         end
       end
@@ -71,12 +81,17 @@ RSpec.describe WorkstationDirector::Director do
         WorkstationDirector::Director.new(actor_class, present_actor_class)
       end
 
-      it 'supports multiple actors' do
-        director.action!
+      it 'reports the actor(s) its running' do
+        expect(STDOUT).to have_received(:puts).with(/Directing these actor(s): ActorClass, PresentActorClass/)
+      end
 
+      it 'creates and directs them all' do
+        expect(actor_class).to have_received(:new)
         expect(actor).to have_received(:present?)
         expect(actor).to have_received(:install)
         expect(actor).to have_received(:setup)
+
+        expect(present_actor_class).to have_received(:new)
         expect(present_actor).to have_received(:present?)
         expect(present_actor).to_not have_received(:install)
         expect(present_actor).to have_received(:setup)
