2ab260328a2e860bf67bff1694ed63e2e2f3f208
Davis W Frank <dwfrank@pivotal.io>
2018-12-15 12:14:28 -0800

    Refactors test
    Authored-by: Davis W Frank <dwfrank@pivotal.io>

--- spec/workstation_director/director_spec.rb
+++ spec/workstation_director/director_spec.rb
@@ -1,22 +1,34 @@
 RSpec.describe WorkstationDirector::Director do
+  let(:actor_class) do
+    actor_class = class_double('ActorClass')
+    allow(actor_class).to receive(:new) {actor}
+    actor_class
+  end
+  let(:actor) do
+    actor = double('Actor')
+    allow(actor).to receive(:install) {true}
+    allow(actor).to receive(:present?) {false}
+    allow(actor).to receive(:setup) {true}
+    actor
+  end
+  let(:present_actor_class) do
+    actor_class = class_double('ActorClass')
+    allow(actor_class).to receive(:new) {present_actor}
+    actor_class
+  end
+  let(:present_actor) do
+    actor = double('Another Actor')
+    allow(actor).to receive(:install) {true}
+    allow(actor).to receive(:present?) {true}
+    allow(actor).to receive(:setup) {true}
+    actor
+  end
 
   context '#action!' do
     context 'with an actor' do
       let(:director) {WorkstationDirector::Director.new(actor_class)}
 
       context 'that returns false from #present?' do
-        let(:actor_class) do
-          actor_class = class_double('ActorClass')
-          allow(actor_class).to receive(:new) {actor}
-          actor_class
-        end
-        let(:actor) do
-          actor = double('Actor')
-          allow(actor).to receive(:install) {true}
-          allow(actor).to receive(:present?) {false}
-          allow(actor).to receive(:setup) {true}
-          actor
-        end
 
         it 'creates the actor' do
           director.action!
@@ -38,61 +50,25 @@ RSpec.describe WorkstationDirector::Director do
       end
 
       context 'that returns true from #present?' do
-        let(:actor_class) do
-          actor_class = class_double('ActorClass')
-          allow(actor_class).to receive(:new) {actor}
-          actor_class
-        end
-        let(:actor) do
-          actor = double('Actor')
-          allow(actor).to receive(:install) {true}
-          allow(actor).to receive(:present?) {true}
-          allow(actor).to receive(:setup) {true}
-          actor
-        end
-
+        let(:director) {WorkstationDirector::Director.new(present_actor_class)}
         it 'does not tell an actor to install' do
           director.action!
 
-          expect(actor).to have_received(:present?)
-          expect(actor).to_not have_received(:install)
+          expect(present_actor).to have_received(:present?)
+          expect(present_actor).to_not have_received(:install)
         end
 
         it 'tells the actor to setup' do
           director.action!
 
-          expect(actor).to have_received(:setup)
+          expect(present_actor).to have_received(:setup)
         end
       end
     end
 
     context 'with multiple actors' do
       let(:director) do
-        WorkstationDirector::Director.new(actor_class, another_actor_class)
-      end
-      let(:actor_class) do
-        actor_class = class_double('ActorClass')
-        allow(actor_class).to receive(:new) {actor}
-        actor_class
-      end
-      let(:actor) do
-        actor = double('Actor')
-        allow(actor).to receive(:install) {true}
-        allow(actor).to receive(:present?) {false}
-        allow(actor).to receive(:setup) {true}
-        actor
-      end
-      let(:another_actor_class) do
-        actor_class = class_double('ActorClass')
-        allow(actor_class).to receive(:new) {another_actor}
-        actor_class
-      end
-      let(:another_actor) do
-        actor = double('Another Actor')
-        allow(actor).to receive(:install) {true}
-        allow(actor).to receive(:present?) {true}
-        allow(actor).to receive(:setup) {true}
-        actor
+        WorkstationDirector::Director.new(actor_class, present_actor_class)
       end
 
       it 'supports multiple actors' do
@@ -101,9 +77,9 @@ RSpec.describe WorkstationDirector::Director do
         expect(actor).to have_received(:present?)
         expect(actor).to have_received(:install)
         expect(actor).to have_received(:setup)
-        expect(another_actor).to have_received(:present?)
-        expect(another_actor).to_not have_received(:install)
-        expect(another_actor).to have_received(:setup)
+        expect(present_actor).to have_received(:present?)
+        expect(present_actor).to_not have_received(:install)
+        expect(present_actor).to have_received(:setup)
       end
     end
 
